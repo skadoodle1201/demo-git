38e928255aafc8be0fed359fd8d9aacfb894be5d
Tanish chaurasia <tanishchaurasia56@gmail.com>
2026-01-07 20:36:44 +0530

    Removed console

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 console.log("Hello world");
-
-console.log("Hello world");
-console.log("Hello world");
-
-console.log("Hello world1");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
-console.log("Hello world");
